8a784d9d8cbe70fd165c615ad38ad9811613cd30
farzad <farzadafi40@gmail.com>
2022-01-30 17:09:05 +0330

    create CategoryService and implement it from Service

---
+++ src/main/java/Service/CategoryService.java
@@ -0,0 +1,27 @@
+package Service;
+
+import Entity.Category;
+
+import java.util.List;
+
+public class CategoryService implements Service<Category> {
+    @Override
+    public int add(Category category) {
+        return 0;
+    }
+
+    @Override
+    public List<Category> findAll() {
+        return null;
+    }
+
+    @Override
+    public int update(Category category) {
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) {
+        return 0;
+    }
+}
