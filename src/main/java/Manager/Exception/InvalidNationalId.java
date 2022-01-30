434aa2e1b49d25ef6a1596e78e3f1192c988a4c2
farzad <farzadafi40@gmail.com>
2022-01-30 18:41:40 +0330

    extend InvalidNationalId form RuntimeException and add method to it for check invalid nationalId

--- src/main/java/Manager/Exception/InvalidNationalId.java
+++ src/main/java/Manager/Exception/InvalidNationalId.java
@@ -1,4 +1,26 @@
 package Manager.Exception;
 
-public class InvalidNationalId {
+public class InvalidNationalId extends RuntimeException {
+    public InvalidNationalId() {
+    }
+
+    public InvalidNationalId(String message) {
+        super(message);
+    }
+
+    public InvalidNationalId(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public void nationalIdChecker(String nationalId){
+        if(nationalId.length() > 10 )
+            throw new InvalidNationalId("national Id can't more than ten number!");
+        if(nationalId.equals(""))
+            throw new InvalidNationalId("dont enter space!");
+        for (Character ch:nationalId.toCharArray()) {
+            if(!Character.isDigit(ch))
+                throw new InvalidNationalId("national Id should be just number!");
+        }
+    }
 }
+
