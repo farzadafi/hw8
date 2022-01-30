3505ddeba77bb20848a39c654450ea9eeab75acd
farzad <farzadafi40@gmail.com>
2022-01-30 18:45:32 +0330

    add method setFullName in utility

--- src/main/java/Manager/Utility.java
+++ src/main/java/Manager/Utility.java
@@ -1,4 +1,27 @@
 package Manager;
 
+import Manager.Exception.InvalidName;
+
+import java.util.Scanner;
+
 public class Utility {
+    private Scanner input = new Scanner(System.in);
+    private String fullName;
+    private InvalidName invalidName = new InvalidName();
+
+
+    public String setFullName(){
+        while(true){
+            System.out.print("Enter name(just alpha):");
+            try {
+                fullName = input.nextLine();
+                invalidName.checkName(fullName);
+                break;
+            }catch (InvalidName except){
+                System.out.println(except.getMessage());
+            }
+        }
+        return fullName;
+    }
+
 }
