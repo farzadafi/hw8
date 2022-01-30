2147e930f12a65351ce069bcbb13ec0be41dba40
farzad <farzadafi40@gmail.com>
2022-01-30 18:41:59 +0330

    extend InvalidName form RuntimeException and add method to it for check invalid name

--- src/main/java/Manager/Exception/InvalidPassword.java
+++ src/main/java/Manager/Exception/InvalidPassword.java
@@ -1,4 +1,41 @@
 package Manager.Exception;
 
-public class InvalidPassword {
+public class InvalidPassword extends RuntimeException {
+    public InvalidPassword() {
+    }
+
+    public InvalidPassword(String message) {
+        super(message);
+    }
+
+    public InvalidPassword(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public void passwordCheck(String password){
+        if(password.length() < 3 )
+            throw new InvalidPassword("password should be more than 2 ");
+        char[] passwordArray = password.toCharArray();
+        char[] signArray =  new char[] {'!','@','#','$','%','^','&','*','(',')','-','+','=','.',',','>','<','?','/','|',':',';'};
+        int space = 0,lowerCase = 0,upperCase = 0,sign = 0,digit = 0;
+        for(int i=0;i<passwordArray.length;i++)
+            if(Character.isSpaceChar(passwordArray[i]))
+                ++space;
+        for(int i = 0;i<passwordArray.length;i++)
+            if(Character.isUpperCase(passwordArray[i]))
+                ++upperCase;
+        for(int i = 0;i<passwordArray.length;i++)
+            if(Character.isLowerCase(passwordArray[i]))
+                ++lowerCase;
+        for(int i = 0;i<passwordArray.length;i++)
+            if(Character.isDigit(passwordArray[i]))
+                ++digit;
+        for(int i=0;i<signArray.length;i++)
+            for(int j=0;j<passwordArray.length;j++)
+                if(signArray[i] == passwordArray[j])
+                    ++sign;
+        if( (space == 0) || (lowerCase == 0) || (upperCase == 0) || (sign == 0) || (digit == 0) )
+            throw new InvalidPassword("password should have space+lowerCase+upperCase+sign+digit!");
+    }
 }
+
