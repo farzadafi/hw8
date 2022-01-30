e20faface1c948b802a65e558ef6eef8fc2ba4b5
farzad <farzadafi40@gmail.com>
2022-01-30 17:08:24 +0330

    create SaleProductService and implement it from Service

---
+++ src/main/java/Service/SaleProductRepository.java
@@ -0,0 +1,27 @@
+package Service;
+
+import Entity.SaleProduct;
+
+import java.util.List;
+
+public class SaleProductRepository implements Service<SaleProduct> {
+    @Override
+    public int add(SaleProduct saleProduct) {
+        return 0;
+    }
+
+    @Override
+    public List<SaleProduct> findAll() {
+        return null;
+    }
+
+    @Override
+    public int update(SaleProduct saleProduct) {
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) {
+        return 0;
+    }
+}
