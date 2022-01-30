aa94bfd7b82eeecc34f134e6436d061cfa5818cc
farzad <farzadafi40@gmail.com>
2022-01-30 18:40:54 +0330

    extend InvalidPassword form RuntimeException and add method to it for check invalid name

--- src/main/java/Manager/Exception/InvalidName.java
+++ src/main/java/Manager/Exception/InvalidName.java
@@ -1,4 +1,31 @@
 package Manager.Exception;
 
-public class InvalidName {
+public class InvalidName extends RuntimeException{
+    public InvalidName() {
+    }
+
+    public InvalidName(String message) {
+        super(message);
+    }
+
+    public InvalidName(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public void checkName(String name){
+        if(name.length() < 3 )
+            throw new InvalidName("name should be more than 2 character!");
+        for (Character ch:name.toCharArray()
+        ) {
+            if(Character.isDigit(ch))
+                throw new InvalidName("name can not have number!");
+        }
+        for (Character ch:name.toCharArray()
+        ) {
+            if(!Character.isAlphabetic(ch))
+                throw new InvalidName("name can't have Sign(!,@,#,%,...)");
+
+        }
+    }
 }
+
