6b4541ae1c2f9705bd2036b57fe3df66c93a5288
farzad <farzadafi40@gmail.com>
2022-01-30 14:18:30 +0330

    create CustomerBasket and all field and all setter , getter

---
+++ src/main/java/Entity/CustomerBasket.java
@@ -0,0 +1,64 @@
+package Entity;
+
+public class CustomerBasket {
+    private int id;
+    private int customerId;
+    private int productId;
+    private int number;
+    private Double totalPrice;
+
+    public CustomerBasket(int id, int customerId, int productId, int number, Double totalPrice) {
+        this.id = id;
+        this.customerId = customerId;
+        this.productId = productId;
+        this.number = number;
+        this.totalPrice = totalPrice;
+    }
+
+    public CustomerBasket(int customerId, int productId, int number, Double totalPrice) {
+        this.customerId = customerId;
+        this.productId = productId;
+        this.number = number;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
