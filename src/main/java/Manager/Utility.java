791cb92f1c761e54cfd741339424aac1e0f0e404
farzad <farzadafi40@gmail.com>
2022-01-30 18:46:52 +0330

    add SetNationalId in Utility

--- src/main/java/Manager/Utility.java
+++ src/main/java/Manager/Utility.java
@@ -1,13 +1,15 @@
 package Manager;
 
 import Manager.Exception.InvalidName;
+import Manager.Exception.InvalidNationalId;
 
 import java.util.Scanner;
 
 public class Utility {
     private Scanner input = new Scanner(System.in);
-    private String fullName;
+    private String fullName,nationalId;
     private InvalidName invalidName = new InvalidName();
+    private InvalidNationalId invalidNationalId = new InvalidNationalId();
 
 
     public String setFullName(){
@@ -23,5 +25,20 @@ public class Utility {
         }
         return fullName;
     }
+
+    public String setNationalId(){
+        while(true){
+            System.out.print("Enter National Id(just number):");
+            try {
+                nationalId = input.nextLine();
+                invalidNationalId.nationalIdChecker(nationalId);
+                break;
+            }catch (InvalidNationalId except){
+                System.out.println(except.getMessage());
+            }
+        }
+        return nationalId;
+    }
+
 
 }
