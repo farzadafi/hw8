97aaa2d95385bd47040af74d185f012e7fc90c2d
farzad <farzadafi40@gmail.com>
2022-01-31 07:28:29 +0330

    add while in App(main)for Run App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,29 @@
+import java.sql.SQLException;
+
 public class App {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        Menu menu = new Menu();
+
+        while(true){
+            switch(menu.publicMenu()){
+                case 1:
+                    //menu.enterMenu();
+                    break;
+
+                case 2:
+                    menu.RegisterMenu();
+                    break;
+
+                case 3:
+                    System.out.println("Have a nice day!");
+                    System.exit(0);
+
+                case 0:
+                    System.out.println("you enter a wrong number!");
+                    break;
+            }
+        }
+    }
 }
+
