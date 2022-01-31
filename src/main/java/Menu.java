5d550ce133e83daa4d87c10240c14f75deee5155
farzad <farzadafi40@gmail.com>
2022-01-31 07:21:56 +0330

    fix RegisterMenu and fix case 1 for add admin

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,9 +1,13 @@
+import Manager.AdminManager;
+
+import java.sql.SQLException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
     private Scanner input = new Scanner(System.in);
     private int command;
+    private AdminManager adminManager = new AdminManager();
 
     //::::>
     public int publicMenu(){
@@ -36,5 +40,41 @@ public class Menu {
                 return 0;
         }
     }
+
+    //:::::>
+    public void RegisterMenu() throws SQLException {
+        System.out.println("Who are you?");
+        System.out.println("1-Admin.");
+        System.out.println("2-Customer.");
+        System.out.println("3-Cancel!");
+        System.out.print("Please select a number:");
+        try {
+            command = input.nextInt();
+        }catch (InputMismatchException exception){
+            input.nextLine();
+            System.out.println("You can just Enter number!");
+            return;
+        }
+        input.nextLine();
+        switch(command)
+        {
+
+            case 1:
+                adminManager.addManager();
+                break;
+
+            case 2:
+                //cinemaManager.registerCinema();
+                break;
+
+            case 3:
+                System.out.println("Good luck!");
+                return;
+
+            default:
+                System.out.println("You enter a wrong number!");
+        }
+    }
+
 
 }//while
