6d738f28abb1b7f0e3f58c5aa50a9abeb3e58603
farzad <farzadafi40@gmail.com>
2022-01-31 13:31:55 +0330

    add setId for id set for add brand

--- src/main/java/Manager/Utility.java
+++ src/main/java/Manager/Utility.java
@@ -1,9 +1,11 @@
 package Manager;
 
+import Entity.Category;
 import Manager.Exception.InvalidName;
 import Manager.Exception.InvalidNationalId;
 import Manager.Exception.InvalidPassword;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Utility {
@@ -68,6 +70,47 @@ public class Utility {
             }
         }
         return categoryName;
+    }
+
+    public int setId(int[] array){
+        int id;
+        System.out.print("Enter your brand id:");
+        try {
+            id = input.nextInt();
+            input.nextLine();
+        }catch (Exception exception){
+            input.nextLine();
+            System.out.println("just Enter number Professor!");
+            return 0;
+        }
+        boolean isContain = false;
+        for (int number:array
+        ) {
+            if(number == id ) {
+                isContain = true;
+                break;
+            }
+        }
+        if(!isContain) {
+            System.out.println("This id is not find!");
+            return 0;
+        }
+        else
+            return id;
+    }
+
+    //::::>
+    public int[] returnIdCategory(List<Category> categoryList){
+        int[] arrayInt = new int[50];
+        int index = 0;
+        for (Category cat:categoryList
+        ) {
+            Category cat1 = new Category();
+            cat1 = cat;
+            arrayInt[index] = cat.getId();
+            index++;
+        }
+        return arrayInt;
     }
 
 
