655f2610db72470577c606d3fb7d08e4d9a69472
farzad <farzadafi40@gmail.com>
2022-01-31 07:23:20 +0330

    fix in AdminService(add method)

---
+++ src/main/java/Service/AdminService.java
@@ -0,0 +1,32 @@
+package Service;
+
+import Entity.Admin;
+import Entity.User;
+import Repository.AdminRepository;
+
+import java.util.List;
+
+public class AdminService implements Service<Admin> {
+    AdminRepository adminRepository = new AdminRepository();
+
+
+    @Override
+    public int add(Admin admin) {
+        return adminRepository.add(admin);
+    }
+
+    @Override
+    public List<Admin> findAll() {
+        return null;
+    }
+
+    @Override
+    public int update(Admin admin) {
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) {
+        return 0;
+    }
+}
