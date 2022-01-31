086aa653d296bcb051bdfe3e1782b6077765ef89
farzad <farzadafi40@gmail.com>
2022-01-31 10:30:28 +0330

    add addCategory methon in AdminManager

--- src/main/java/Manager/AdminManager.java
+++ src/main/java/Manager/AdminManager.java
@@ -1,15 +1,18 @@
 package Manager;
 
 import Entity.Admin;
+import Entity.Category;
 import Entity.TypeUser;
 import Service.AdminService;
+import Service.CategoryService;
 import Service.LoginService;
 
 public class AdminManager {
     private AdminService adminService = new AdminService();
-    private String fullName,nationalId,password;
+    private String fullName,nationalId,password,categoryName;
     private Utility utility = new Utility();
     private LoginService loginService = new LoginService();
+    private CategoryService categoryService = new CategoryService();
 
     //::::>
     public void addManager(){
@@ -36,5 +39,14 @@ public class AdminManager {
         if(adminService.update(admin) != 0 ){
             System.out.println(fullName + " successful updated!");
         }
+    }
+
+    public void addCategory(){
+        categoryName = utility.setCategoryName();
+        Category category = new Category(0,categoryName);
+        if(categoryService.add(category) != 0 )
+            System.out.println(categoryName + " successful added!");
+        else
+            System.out.println("Something is wrong");
     }
 }
