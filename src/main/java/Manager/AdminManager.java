c7e278bc8f843b501655123f340172420fb3d5dd
farzad <farzadafi40@gmail.com>
2022-01-31 07:22:46 +0330

    add in AdminManager(addManager method)

--- src/main/java/Manager/AdminManager.java
+++ src/main/java/Manager/AdminManager.java
@@ -1,4 +1,30 @@
 package Manager;
 
+import Entity.Admin;
+import Entity.TypeUser;
+import Service.AdminService;
+import Service.LoginService;
+
 public class AdminManager {
+    private AdminService adminService = new AdminService();
+    private String fullName,nationalId,password;
+    private Utility utility = new Utility();
+    private LoginService loginService = new LoginService();
+
+
+    public void addManager(){
+        fullName= utility.setFullName();
+        while(true){
+            nationalId = utility.setNationalId();
+            if(loginService.returnUser(nationalId) == null)
+                break;
+            else
+                System.out.println("this national Id is used before!");
+        }
+        password = utility.setPassword();
+        Admin admin = new Admin(fullName,nationalId,password,TypeUser.ADMIN);
+        if(adminService.add(admin) != 0 ){
+            System.out.println(fullName + " successful added!");
+        }
+    }
 }
