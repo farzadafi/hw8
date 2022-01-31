06226525abaa62320b7a48115cbc777474c84941
farzad <farzadafi40@gmail.com>
2022-01-31 18:24:19 +0330

    add int brandId and setter , getter

--- src/main/java/Entity/Product.java
+++ src/main/java/Entity/Product.java
@@ -4,22 +4,25 @@ public class Product {
     private int id;
     private int adminId;
     private int categoryId;
+    private int brandId;
     private String name;
     private int number;
     private Double price;
 
-    public Product(int id, int adminId,int categoryId, String name, int number, Double price) {
+    public Product(int id, int adminId,int categoryId,int brandId, String name, int number, Double price) {
         this.id = id;
         this.adminId = adminId;
         this.categoryId = categoryId;
+        this.brandId = brandId;
         this.name = name;
         this.number = number;
         this.price = price;
     }
 
-    public Product(int adminId,int categoryId, String name, int number, Double price) {
+    public Product(int adminId,int categoryId,int brandId, String name, int number, Double price) {
         this.adminId = adminId;
         this.categoryId = categoryId;
+        this.brandId = brandId;
         this.name = name;
         this.number = number;
         this.price = price;
@@ -71,5 +74,13 @@ public class Product {
 
     public void setCategoryId(int categoryId) {
         this.categoryId = categoryId;
+    }
+
+    public int getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(int brandId) {
+        this.brandId = brandId;
     }
 }
